af55d35acdc23b911e8ea5a933619dd7c760405f
Carlos-Silva <carlosgsilva.dev@gmail.com>
2021-04-21 19:17:49 -0400

    Configurando o express e criando a rota POST

---
+++ controllers/atendimentos.js
@@ -0,0 +1,9 @@
+module.exports = app =>  {
+    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimento e está realizando um GET'))
+
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+        res.send('Você está na rota de atendimento e está realizando um POST')
+    })
+
+}
