6e030a18d73a71f46f364f7dc577b7e82d484d3e
Carlos-Silva <carlosgsilva.dev@gmail.com>
2021-04-21 23:54:55 -0400

    Criando a rota para realizar alterações na base de dados 'PATCH'

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -31,19 +31,73 @@ class Atendimento {
             res.status(400).json(erros)
         } else {
             const atendimentoDatado = {...atendimento, dataCriacao, data}
-            
+
             const sql = 'INSERT INTO Atendimentos SET ?'
 
             conexao.query(sql, atendimentoDatado, (erro, resultados) => {
                 if(erro) {
                     res.status(400).json(erro)
                 } else {
-                    res.status(201).json(resultados)
+                    res.status(201).json(atendimento)
                 }
             })
         }
 
         
+    }
+
+    lista(res) {
+        const sql = 'SELECT * FROM atendimentos'
+
+        conexao.query(sql, (erro, resultados) => {
+            if(erro){
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(resultados)
+            }
+        })
+    }
+
+    buscaPorId(id, res) {
+        const sql = `SELECT * FROM Atendimentos WHERE id = ${id}`
+
+        conexao.query(sql, (erro, resultados) => {
+            const atendimento = resultados[0]
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(atendimento)
+            }
+        })
+    }
+
+    altera(id, valores, res) {
+
+        if(valores.data) {
+            valores.data = moment(valores.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        }
+
+        const sql = 'UPDATE Atendimentos SET ? WHERE id=?'
+
+        conexao.query(sql, [valores, id], (erro, resultados) => {
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(...valores, id)
+            }
+        })
+    }
+
+    deleta(id, res) {
+        const sql = 'DELETE FROM Atendimentos WHERE id=?'
+
+        conexao.query(sql, id, (erro, resultados) => {
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json({id})
+            }
+        })
     }
 }
 
