64fd99029e8f229b01d5a3dfa95d020bb7512068
Carlos-Silva <carlosgsilva.dev@gmail.com>
2021-04-21 20:17:36 -0400

    Conectando o servidor com o mysql e criando as tabelas no banco de dados usando um script node

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,9 +1,14 @@
+const Atendimento = require('../models/atendimentos')
+
+
 module.exports = app =>  {
     app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimento e está realizando um GET'))
 
     app.post('/atendimentos', (req, res) => {
-        console.log(req.body)
-        res.send('Você está na rota de atendimento e está realizando um POST')
+        const atendimento = req.body
+
+        Atendimento.adiciona(atendimento)
+        res.send('Post atendimento')
     })
 
 }
--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const customExpress = require('./config/customExpress')
+const conexao = require('./infraestrutura/conexao')
+const Tabelas = require('./infraestrutura/tabelas')
 
-const app = customExpress()
+conexao.connect(erro => {
+    if(erro){
+        console.log(erro)
+    } else {
+        console.log('Conectado com sucesso!')
+        Tabelas.init(conexao)
+        const app = customExpress()
+        app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
+    }
+})
 
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
+
