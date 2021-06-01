d39d713d21b4c164011c7dee3f71d285f2059b65
carlosgsilva <carlosgsilva.dev@gmail.com>
2021-05-31 22:52:51 -0400

    Segurança ao receber dados externos e Lidando com erros no tipo de imagens

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,11 +1,23 @@
 const fs = require('fs')
+const path = require('path')
 
 module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => 
 {
-    const novoCaminho = `./assets/imagens/${nomeDoArquivo}`
+    const tiposValidos = ['jpg', 'png', 'jpeg']
+    const tipo = path.extname(caminho)
+    const tipoEhValido = tiposValidos.indexOf(tipo.substring(1)) !== -1
 
-    fs.createReadStream(caminho)
-    .pipe(fs.createWriteStream(novoCaminho))
-    .on('finish', () => callbackImagemCriada(novoCaminho))
+    if(tipoEhValido) {
+        const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`   
+        
+        fs.createReadStream(caminho)
+        .pipe(fs.createWriteStream(novoCaminho))
+        .on('finish', () => callbackImagemCriada(false, novoCaminho))
+    } else {
+        const erro = 'Tipo é inválido'
+        console.log('Erro! Tipo Inválido')
+        callbackImagemCriada(erro)
+    }
+
 }
     
--- models/pets.js
+++ models/pets.js
@@ -5,17 +5,22 @@ class Pet {
   adiciona(pet, res) {
     const query = 'INSERT INTO Pets SET ?'
 
-    uploadDeArquivo(pet.imagem, pet.nome, (novoCaminho) => {
-      const novoPet = {nome: pet.nome, imagem: novoCaminho }
+    uploadDeArquivo(pet.imagem, pet.nome, (erro, novoCaminho) => {
 
-      conexao.query(query, novoPet, erro => {
-        if(erro) {
-          console.log(erro)
-          res.status(400).json(erro)
-        } else {
-          res.status(200).json(novoPet)
-        }
-      })
+      if(erro) {
+        res.status(400).json({erro})
+      } else {
+        const novoPet = {nome: pet.nome, imagem: novoCaminho }
+
+        conexao.query(query, novoPet, erro => {
+          if(erro) {
+            console.log(erro)
+            res.status(400).json(erro)
+          } else {
+            res.status(200).json(novoPet)
+          }
+        })
+      }
     })
    }
 }
